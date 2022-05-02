9e7739433b505a8d00e20ae6bf10b5f6459cf027
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-05-01 23:46:07 -0500

    Add pagination

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import {loadFavourites, loadImgRandom, uploadDogPhoto} from './utils/fetchDog.js';
 
-const btn_uploadDog = document.querySelector('#uploadingForm button')
-
-btn_uploadDog.addEventListener('click',uploadDogPhoto)
-
 
 loadImgRandom()
-loadFavourites();
+//loadFavourites();
--- src/utils/view.js
+++ src/utils/view.js
@@ -1,12 +1,140 @@
 import * as API from './fetchDog.js';
 
-const img = document.querySelectorAll('#randomDog img');
 const spanError = document.getElementById('error');
-const randomDogSection = document.getElementById('randomDog'); 
+let randomDogSection ; 
+let favouriteDogList ;
+let formUploadElement;
+const containerElement = document.getElementById('container');
+const pageAllElement = document.getElementById('page-all');
+const pageFavouritesElement = document.getElementById('page-favourites');
+const pageUploadElement = document.getElementById('page-upload');
+
+pageAllElement.addEventListener('click',(e)=>{
+    containerElement.innerHTML = '';
+    pageAllElement.classList.add('page-focused');
+    pageAllElement.classList.remove('page-blurred');
+
+    pageFavouritesElement.classList.add('page-blurred');
+    pageFavouritesElement.classList.remove('page-focused');
+
+    pageUploadElement.classList.add('page-blurred');
+    pageUploadElement.classList.remove('page-focused');
+    createAllPage();
+    API.loadImgRandom();
+});
+pageFavouritesElement.addEventListener('click',(e)=>{
+    containerElement.innerHTML = '';
+    pageFavouritesElement.classList.add('page-focused');
+    pageFavouritesElement.classList.remove('page-blurred');
+
+    pageAllElement.classList.add('page-blurred');
+    pageAllElement.classList.remove('page-focused');
+    
+    pageUploadElement.classList.add('page-blurred');
+    pageUploadElement.classList.remove('page-focused');
+    createfavouritePage();
+    API.loadFavourites();
+});
+pageUploadElement.addEventListener('click',(e)=>{
+    containerElement.innerHTML = '';
+    pageUploadElement.classList.add('page-focused');
+    pageUploadElement.classList.remove('page-blurred');
+
+    pageFavouritesElement.classList.add('page-blurred');
+    pageFavouritesElement.classList.remove('page-focused');
+
+    pageAllElement.classList.add('page-blurred');
+    pageAllElement.classList.remove('page-focused');
+    
+    createuploadPage()
+});
+
+function createuploadPage(){
+    const div = document.createElement('div');
+    
+    let section = document.createElement('section');
+    section.classList.add('uploadingDog');
+    let h2 = document.createElement('h2');
+    h2.innerHTML = "Sube la foto de tu Perrito"
+    formUploadElement = document.createElement('form');
+    formUploadElement.id = 'uploadingForm';
+    let inputFile = document.createElement('input');
+    inputFile.type = 'file';
+    inputFile.id = 'file';
+    inputFile.name = 'file';
+    let button = document.createElement('button');
+    button.innerHTML = 'Subir foto de tu perro';
+    button.addEventListener('click',API.uploadDogPhoto)
+    formUploadElement.appendChild(inputFile);
+    formUploadElement.appendChild(button);
+    section.appendChild(h2);
+    section.appendChild(formUploadElement);
+
+    div.appendChild(section);
+    containerElement.appendChild(div)
+}
+
+function createfavouritePage(){
+    const div = document.createElement('div');
+    div.classList.add('favouritesGallery');
+    let h2 = document.createElement('h2');
+    h2.innerText = 'Favoritos';
+    //h2.classList.add('m-5')
+    let section = document.createElement('section');
+    section.id ='favouritesDogs';
+    section.classList.add('flex', 'justify-center');
+    favouriteDogList = document.createElement('div');
+    favouriteDogList.id = 'favouriteDogList';
+    favouriteDogList.classList.add('container-dogs');
+    section.appendChild(favouriteDogList);
+
+    div.appendChild(h2);
+    div.appendChild(section);
+    
+    containerElement.appendChild(div);
+}
+
+function createAllPage(){
+    const div = document.createElement('div');
+    div.classList.add('flex','justify-center');
+    div.innerHTML = getFilterImageElement()
+    
+
+    containerElement.appendChild(div);
+    containerElement.appendChild(getGalleryRandomElement())
+}
+
+function getGalleryRandomElement(){
+    const div = document.createElement('div');
+    div.classList.add('flex','justify-center');
+    randomDogSection = document.createElement('section');
+    randomDogSection.id = 'randomDog';
+    randomDogSection.classList.add('container-dogs');
+    div.appendChild(randomDogSection)
+    return div;
+}
+
+function getFilterImageElement(){
+    return `
+        <div class="w-11/12 my-3 flex items-center ">
+            <label class="flex items-center gap-4">
+            <span class="text-gray-700 font-semibold">Tipo</span>
+            <select id="select-type-dog"
+                class="bg-transparent block w-28 mt-0 px-1.5 border-0 border-b-2 border-gray-300 text-gray-500  focus:ring-0 focus:border-black">
+                <option value="all">Todo</option>
+                <option value="static">Est&aacute;tico</option>
+                <option value="animated">Animado</option>
+            </select>
+            </label>
+            <div class="h-full px-4 flex items-center flex-nowrap gap-6 overflow-x-scroll scrollbar-none">
+            <span class="item-breeds">GUILLERMO</span>
+            </div>
+        </div>
+    `
+}
 
 function getFromUpload(){
-    const form = document.getElementById('uploadingForm');
-    return new FormData(form);
+    return new FormData(formUploadElement);
 }
 
 function errorMessage(error, message){
@@ -14,7 +142,6 @@ function errorMessage(error, message){
 }
 
 function drawFavourites(dogList){
-    let favouriteDogList = document.getElementById('favouriteDogList');
     favouriteDogList.innerHTML = '';
     const docFra = document.createDocumentFragment();
     dogList.forEach(dog =>{
@@ -59,4 +186,5 @@ function getElementImage(dog, buttonName, callback){
     return div
 }
 
+createAllPage()
 export{drawFavourites, drawImgRandom, errorMessage, getFromUpload}
